34dbd3733834b2e1984b7efe1d2dd02c2b71350f
Dimitris Tselikis <dimitriostselikis@gmail.com>
2022-08-10 15:57:28 +0300

    Implemented basic profile fragment's functionality

---
+++ app/src/main/java/com/homelab/appointment/ui/hub/profile/ProfileFragment.kt
@@ -0,0 +1,38 @@
+package com.homelab.appointment.ui.hub.profile
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import com.homelab.appointment.R
+import com.homelab.appointment.databinding.FragmentProfileBinding
+
+class ProfileFragment : Fragment() {
+
+    private val viewModel: ProfileViewModel by viewModels()
+
+    private lateinit var binding: FragmentProfileBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_profile, null, false)
+
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.apply {
+            lifecycleOwner = viewLifecycleOwner
+            viewModel = this@ProfileFragment.viewModel
+            profileFragment = this@ProfileFragment
+        }
+    }
+
+}
