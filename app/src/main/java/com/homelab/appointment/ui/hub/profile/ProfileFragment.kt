750ac3511604cecf34113d4cf4a52056a061a5f9
Dimitris Tselikis <dimitriostselikis@gmail.com>
2022-08-10 16:08:36 +0300

    Implemented profile viewmodel

--- app/src/main/java/com/homelab/appointment/ui/hub/profile/ProfileFragment.kt
+++ app/src/main/java/com/homelab/appointment/ui/hub/profile/ProfileFragment.kt
@@ -6,13 +6,18 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
 import androidx.fragment.app.viewModels
 import com.homelab.appointment.R
 import com.homelab.appointment.databinding.FragmentProfileBinding
+import com.homelab.appointment.ui.hub.HubSharedViewModel
 
 class ProfileFragment : Fragment() {
 
-    private val viewModel: ProfileViewModel by viewModels()
+    private val sharedViewModel: HubSharedViewModel by activityViewModels()
+    private val viewModel: ProfileViewModel by viewModels {
+        ProfileViewModelFactory(sharedViewModel.user)
+    }
 
     private lateinit var binding: FragmentProfileBinding
 
---
+++ app/src/main/java/com/homelab/appointment/ui/hub/profile/ProfileViewModel.kt
@@ -0,0 +1,13 @@
+package com.homelab.appointment.ui.hub.profile
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.homelab.appointment.model.User
+
+class ProfileViewModel(user: User) : ViewModel() {
+    val firstname = MutableLiveData<String>(user.firstname)
+    val lastname = MutableLiveData<String>(user.lastname)
+    val phone = MutableLiveData<String>(user.phone)
+    val email = MutableLiveData<String>(user.email)
+    val fbName = MutableLiveData<String>(user.fbName)
+}
