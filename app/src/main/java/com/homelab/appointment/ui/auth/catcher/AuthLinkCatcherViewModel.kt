f96e94240c5a0a465c4c2c3b310009a3bddbe6d5
Dimitris Tselikis <dimitriostselikis@gmail.com>
2022-08-16 18:34:45 +0300

    Update email if user already exists

---
+++ app/src/main/java/com/homelab/appointment/ui/auth/catcher/AuthLinkCatcherViewModel.kt
@@ -0,0 +1,28 @@
+package com.homelab.appointment.ui.auth.catcher
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+import com.homelab.appointment.data.USERS_COLLECTION
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.SharedFlow
+import kotlinx.coroutines.launch
+
+class AuthLinkCatcherViewModel : ViewModel() {
+    private val _emailUpdated = MutableSharedFlow<Boolean>()
+    val emailUpdated: SharedFlow<Boolean> = _emailUpdated
+
+    fun updateEmail() {
+        FirebaseAuth.getInstance().currentUser?.let {
+            Firebase.firestore.collection(USERS_COLLECTION).document(it.uid)
+                .update(mapOf("email" to it.email))
+                .addOnCompleteListener { task ->
+                    viewModelScope.launch {
+                        _emailUpdated.emit(task.isSuccessful)
+                    }
+                }
+        }
+    }
+}
