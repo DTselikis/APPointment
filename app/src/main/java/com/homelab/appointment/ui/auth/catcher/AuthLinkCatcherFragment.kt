955c02195b85ededdc41f59fe5c76b80a5857e4e
Dimitris Tselikis <dimitriostselikis@gmail.com>
2022-08-16 18:48:02 +0300

    Navigate to profile if new email stored

--- app/src/main/java/com/homelab/appointment/ui/auth/catcher/AuthLinkCatcherFragment.kt
+++ app/src/main/java/com/homelab/appointment/ui/auth/catcher/AuthLinkCatcherFragment.kt
@@ -2,15 +2,21 @@ package com.homelab.appointment.ui.auth.catcher
 
 import android.net.Uri
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
 import com.homelab.appointment.R
 import com.homelab.appointment.data.LinkType
+import kotlinx.coroutines.flow.collectLatest
 
 class AuthLinkCatcherFragment : Fragment() {
+
+    private val viewModel: AuthLinkCatcherViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -22,6 +28,8 @@ class AuthLinkCatcherFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        observeEmailUpdated()
 
         handleLink(requireActivity().intent.data!!)
     }
@@ -38,8 +46,19 @@ class AuthLinkCatcherFragment : Fragment() {
         }
     }
 
+    private fun observeEmailUpdated() {
+        viewLifecycleOwner.lifecycleScope.launchWhenStarted {
+            viewModel.emailUpdated.collectLatest { updated ->
+                if (updated) {
+                    navigateToHub()
+                }
+            }
+        }
+    }
+
     private fun navigateToHub() {
         // TODO navigate to Hub
+        Log.d("HIT", "navigateToHub: HIT")
     }
 
     private fun navigateToProfile() {
