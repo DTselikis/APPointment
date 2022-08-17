1a3272dba60f1405fba24d9f9edfbdff9ca21149
Dimitris Tselikis <dimitriostselikis@gmail.com>
2022-08-17 12:18:45 +0300

    Check if email verified

--- app/src/main/java/com/homelab/appointment/ui/auth/verification/EmailVerificationFragment.kt
+++ app/src/main/java/com/homelab/appointment/ui/auth/verification/EmailVerificationFragment.kt
@@ -6,12 +6,17 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.navArgs
+import com.google.android.material.snackbar.Snackbar
 import com.homelab.appointment.R
 import com.homelab.appointment.databinding.FragmentEmailVerificationBinding
+import kotlinx.coroutines.flow.collectLatest
 
 class EmailVerificationFragment : Fragment() {
     private val args: EmailVerificationFragmentArgs by navArgs<EmailVerificationFragmentArgs>()
+    private val viewModel: EmailVerificationViewModel by viewModels()
 
     private lateinit var binding: FragmentEmailVerificationBinding
 
@@ -19,7 +24,12 @@ class EmailVerificationFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_email_verification, container, false)
+        binding = DataBindingUtil.inflate(
+            inflater,
+            R.layout.fragment_email_verification,
+            container,
+            false
+        )
 
         return binding.root
     }
@@ -30,6 +40,30 @@ class EmailVerificationFragment : Fragment() {
         binding.apply {
             lifecycleOwner = viewLifecycleOwner
             emailVerificationText.text = args.user.email
+            emailVerificationFragment = this@EmailVerificationFragment
+        }
+
+        observeEmailVerified()
+    }
+
+    fun checkEmailVerified() {
+        viewModel.checkEmailVerified()
+    }
+
+    private fun observeEmailVerified() {
+        viewLifecycleOwner.lifecycleScope.launchWhenStarted {
+            viewModel.emailVerified.collectLatest { verified ->
+                if (verified) {
+                    // TODO navigate to hub
+                } else {
+                    Snackbar.make(
+                        binding.emailVerificationIcon,
+                        getString(R.string.verification_dialog_err_txt),
+                        Snackbar.LENGTH_SHORT
+                    )
+                        .show()
+                }
+            }
         }
     }
 }
