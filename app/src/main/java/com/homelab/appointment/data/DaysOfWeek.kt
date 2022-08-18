8057571a744c04e4a7e96acc19a3881ae1ff19a0
Dimitris Tselikis <dimitriostselikis@gmail.com>
2022-08-18 13:15:49 +0300

    Added days of week map

---
+++ app/src/main/java/com/homelab/appointment/data/DaysOfWeek.kt
@@ -0,0 +1,13 @@
+package com.homelab.appointment.data
+
+import com.homelab.appointment.R
+
+val daysOfWeek = mapOf(
+    DayOfWeek.MONDAY to R.string.monday,
+    DayOfWeek.TUESDAY to R.string.tuesday,
+    DayOfWeek.WEDNESDAY to R.string.wednesday,
+    DayOfWeek.THURSDAY to R.string.thursday,
+    DayOfWeek.FRIDAY to R.string.friday,
+    DayOfWeek.SATURDAY to R.string.saturday,
+    DayOfWeek.SUNDAY to R.string.sunday
+)
