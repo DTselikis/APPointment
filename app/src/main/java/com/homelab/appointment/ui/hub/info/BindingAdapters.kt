32a1f0ad3aaa07da622a33db2d955d11586b01d4
Dimitris Tselikis <dimitriostselikis@gmail.com>
2022-08-19 15:27:51 +0300

    Added opening hours

--- app/src/main/java/com/homelab/appointment/ui/hub/info/BindingAdapters.kt
+++ app/src/main/java/com/homelab/appointment/ui/hub/info/BindingAdapters.kt
@@ -3,6 +3,7 @@ package com.homelab.appointment.ui.hub.info
 import androidx.databinding.BindingAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.textview.MaterialTextView
+import com.homelab.appointment.R
 import com.homelab.appointment.data.daysOfWeek
 import com.homelab.appointment.data.orderToDayOfWeek
 import com.homelab.appointment.model.helper.DayOpeningHours
@@ -21,4 +22,25 @@ fun bindDate(materialTextView: MaterialTextView, day: Int?) {
    val dayOfWeek = orderToDayOfWeek[day]
 
     materialTextView.text = materialTextView.resources.getString(daysOfWeek[dayOfWeek]!!)
+}
+
+@BindingAdapter("hours")
+fun bindHours(materialTextView: MaterialTextView, dayOpeningHours: DayOpeningHours?) {
+    val resources = materialTextView.resources
+
+    var morningHours: String? = null
+    dayOpeningHours!!.morning?.let {
+        morningHours = resources.getString(R.string.opening_hours, it.from, it.to)
+    }
+
+    var afternoonHours: String? = null
+    dayOpeningHours.afternoon?.let {
+        afternoonHours = resources.getString(R.string.opening_hours, it.from, it.to)
+    }
+
+    materialTextView.text = if (morningHours == null && afternoonHours == null) {
+        resources.getString(R.string.store_closed)
+    } else {
+        "${morningHours ?: ""}\n${afternoonHours ?: ""}"
+    }
 }
