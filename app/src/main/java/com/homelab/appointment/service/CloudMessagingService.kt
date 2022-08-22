1770d5b5caf68ef0f480549e696c6eda7e0455d7
Dimitris Tselikis <dimitriostselikis@gmail.com>
2022-08-22 19:26:48 +0300

    Create notification channel

--- app/src/main/java/com/homelab/appointment/service/CloudMessagingService.kt
+++ app/src/main/java/com/homelab/appointment/service/CloudMessagingService.kt
@@ -1,15 +1,19 @@
 package com.homelab.appointment.service
 
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
 import android.content.Context
+import android.graphics.Color
+import android.os.Build
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 import com.google.firebase.messaging.FirebaseMessagingService
-import com.homelab.appointment.data.SHARED_PREF_FCM_KEY
-import com.homelab.appointment.data.SHARED_PREF_NAME
-import com.homelab.appointment.data.USERS_COLLECTION
-import com.homelab.appointment.data.USER_DOCUMENT_TOKEN_FIELD
+import com.google.firebase.messaging.RemoteMessage
+import com.homelab.appointment.R
+import com.homelab.appointment.data.*
 
 class CloudMessagingService : FirebaseMessagingService() {
     override fun onNewToken(token: String) {
@@ -22,6 +26,12 @@ class CloudMessagingService : FirebaseMessagingService() {
         } else {
             storeFcmTokenToSharedPref(token)
         }
+    }
+
+    override fun onMessageReceived(message: RemoteMessage) {
+        super.onMessageReceived(message)
+
+        createNotificationChannel()
     }
 
     private fun storeFcmTokenToDb(firebaseUser: FirebaseUser, token: String) {
@@ -39,6 +49,25 @@ class CloudMessagingService : FirebaseMessagingService() {
         with(sharedPref.edit()) {
             putString(SHARED_PREF_FCM_KEY, token)
             apply()
+        }
+    }
+
+    private fun createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val name = getString(R.string.notification_channe_name)
+            val importance = NotificationManager.IMPORTANCE_HIGH
+            val channel = NotificationChannel(CHANNEL_ID, name, importance).apply {
+                description = getString(R.string.notification_channel_descr)
+                setShowBadge(true)
+                enableLights(true)
+                enableVibration(true)
+                lightColor = Color.GREEN
+                lockscreenVisibility = Notification.VISIBILITY_SECRET
+            }
+
+            val notificationManager =
+                getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(channel)
         }
     }
 
