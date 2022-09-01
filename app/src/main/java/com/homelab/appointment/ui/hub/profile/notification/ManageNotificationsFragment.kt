531e5951d47ed6c4392106a2d2150e436a153ca2
Dimitris Tselikis <dimitriostselikis@gmail.com>
2022-09-02 02:05:39 +0300

    Implemented recycler view swipe to delete

--- app/src/main/java/com/homelab/appointment/ui/hub/profile/notification/ManageNotificationsFragment.kt
+++ app/src/main/java/com/homelab/appointment/ui/hub/profile/notification/ManageNotificationsFragment.kt
@@ -1,6 +1,9 @@
 package com.homelab.appointment.ui.hub.profile.notification
 
 import android.app.Dialog
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.os.Build
 import android.os.Bundle
 import android.util.TypedValue
@@ -10,14 +13,18 @@ import android.view.ViewGroup
 import android.widget.FrameLayout
 import androidx.annotation.ColorRes
 import androidx.core.content.ContextCompat
+import androidx.core.content.res.ResourcesCompat
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.navArgs
+import androidx.recyclerview.widget.ItemTouchHelper
+import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.bottomsheet.BottomSheetBehavior
 import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import com.homelab.appointment.R
 import com.homelab.appointment.databinding.FragmentManageNotificationsBinding
+import kotlin.math.roundToInt
 
 
 class ManageNotificationsFragment : BottomSheetDialogFragment() {
@@ -34,6 +41,8 @@ class ManageNotificationsFragment : BottomSheetDialogFragment() {
 
     private lateinit var bottomSheetDialog: BottomSheetDialog
     private lateinit var bottomSheetBehavior: BottomSheetBehavior<FrameLayout>
+
+    private lateinit var swipeHelper: ItemTouchHelper
 
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
         bottomSheetDialog = super.onCreateDialog(savedInstanceState) as BottomSheetDialog
@@ -96,7 +105,107 @@ class ManageNotificationsFragment : BottomSheetDialogFragment() {
 
 
         viewModel.fetchNotifications(args.uid)
+
+        setupSwipeToDelete()
     }
+
+    // Code from https://medium.com/getpowerplay/understanding-swipe-and-drag-gestures-in-recyclerview-cb3136beff20
+    // https://github.com/ishanknijhawan/SwipeGestures
+    // and for background color and icon https://medium.com/@zackcosborn/step-by-step-recyclerview-swipe-to-delete-and-undo-7bbae1fce27e
+    private fun setupSwipeToDelete() {
+        val height: Int
+        val width: Int
+        resources.displayMetrics.let { metrics ->
+            height = (metrics.heightPixels / metrics.density).toInt().dp
+            width = (metrics.widthPixels / metrics.density).toInt().dp
+        }
+
+        val deleteIcon = ResourcesCompat.getDrawable(
+            resources,
+            R.drawable.ic_delete_forever_30,
+            requireActivity().theme
+        )
+        val deleteColor =
+            ResourcesCompat.getColor(resources, R.color.email_red, requireActivity().theme)
+
+        swipeHelper = ItemTouchHelper(object :
+            ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT or ItemTouchHelper.RIGHT) {
+            override fun onMove(
+                recyclerView: RecyclerView,
+                viewHolder: RecyclerView.ViewHolder,
+                target: RecyclerView.ViewHolder
+            ): Boolean = true
+
+            override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+                TODO("Not yet implemented")
+            }
+
+            override fun onChildDraw(
+                canvas: Canvas,
+                recyclerView: RecyclerView,
+                viewHolder: RecyclerView.ViewHolder,
+                dX: Float,
+                dY: Float,
+                actionState: Int,
+                isCurrentlyActive: Boolean
+            ) {
+                super.onChildDraw(
+                    canvas,
+                    recyclerView,
+                    viewHolder,
+                    dX,
+                    dY,
+                    actionState,
+                    isCurrentlyActive
+                )
+
+                val background = ColorDrawable(Color.RED)
+                val itemView = viewHolder.itemView
+                val backgroundCornerOffset = 20
+                val iconMargin = (itemView.height - deleteIcon!!.intrinsicHeight) / 2
+                val iconTop = itemView.top + (itemView.height - deleteIcon.intrinsicHeight) / 2
+                val iconBottom = iconTop + deleteIcon.intrinsicHeight
+
+                if (dX > 0) { // Swiping to the right
+                    val iconLeft = itemView.left + iconMargin
+                    val iconRight = itemView.left + iconMargin + deleteIcon.intrinsicWidth
+                    deleteIcon.setBounds(iconLeft, iconTop, iconRight, iconBottom)
+
+                    background.setBounds(
+                        itemView.left,
+                        itemView.top,
+                        itemView.left + dX.roundToInt() + backgroundCornerOffset,
+                        itemView.bottom
+                    )
+                } else if (dX < 0) { // Swiping to the left
+                    val iconLeft = itemView.right - iconMargin - deleteIcon.intrinsicHeight
+                    val iconRight = itemView.right - iconMargin
+                    deleteIcon.setBounds(iconLeft, iconTop, iconRight, iconBottom)
+
+                    background.setBounds(
+                        itemView.right + dX.roundToInt() - backgroundCornerOffset,
+                        itemView.top,
+                        itemView.right,
+                        itemView.bottom
+                    )
+                } else { // View is unSwiped
+                    background.setBounds(0, 0, 0, 0)
+                }
+
+                background.draw(canvas)
+                deleteIcon.draw(canvas)
+            }
+
+        })
+
+        swipeHelper.attachToRecyclerView(binding.notificationsRv)
+    }
+
+    private val Int.dp
+        get() = TypedValue.applyDimension(
+            TypedValue.COMPLEX_UNIT_DIP,
+            toFloat(), resources.displayMetrics
+        ).roundToInt()
 
     fun getColor(@ColorRes color: Int): Int {
         return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
