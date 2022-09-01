919338530019b59e5b6739edcaa6e828cdaa9fbc
Dimitris Tselikis <dimitriostselikis@gmail.com>
2022-09-01 15:42:06 +0300

    Implemented ManageNotificationsFragment

---
+++ app/src/main/java/com/homelab/appointment/ui/hub/profile/notification/ManageNotificationsFragment.kt
@@ -0,0 +1,34 @@
+package com.homelab.appointment.ui.hub.profile.notification
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import com.homelab.appointment.R
+import com.homelab.appointment.databinding.FragmentManageNotificationsBinding
+
+
+class ManageNotificationsFragment : Fragment() {
+
+    private lateinit var binding: FragmentManageNotificationsBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_manage_notifications, container, false)
+
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.apply {
+            lifecycleOwner = viewLifecycleOwner
+        }
+    }
+
+}
