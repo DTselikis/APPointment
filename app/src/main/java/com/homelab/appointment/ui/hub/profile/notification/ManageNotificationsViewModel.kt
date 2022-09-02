bc84ddfbdbdd96473d95f2f4ead44b5ebc9ef978
Dimitris Tselikis <dimitriostselikis@gmail.com>
2022-09-02 18:09:31 +0300

    Update active notifications count

--- app/src/main/java/com/homelab/appointment/ui/hub/profile/notification/ManageNotificationsViewModel.kt
+++ app/src/main/java/com/homelab/appointment/ui/hub/profile/notification/ManageNotificationsViewModel.kt
@@ -8,8 +8,10 @@ import com.google.firebase.firestore.SetOptions
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.firestore.ktx.toObject
 import com.google.firebase.ktx.Firebase
+import com.homelab.appointment.data.ACTIVE_NOTIFICATIONS_FIELD
 import com.homelab.appointment.data.NOTIFICATIONS_COLLECTION
 import com.homelab.appointment.data.NOTIFICATION_FIELD
+import com.homelab.appointment.data.USERS_COLLECTION
 import com.homelab.appointment.model.Notification
 import com.homelab.appointment.model.helper.NotificationsList
 import kotlinx.coroutines.Dispatchers
@@ -46,11 +48,23 @@ class ManageNotificationsViewModel : ViewModel() {
 
     fun deleteOldNotifications(uid: String) {
         remainingNotifications?.let {
-            Firebase.firestore.collection(NOTIFICATIONS_COLLECTION).document(uid).apply {
-                if (it.isEmpty()) {
-                    delete()
-                } else {
-                    set(mapOf(NOTIFICATION_FIELD to remainingNotifications), SetOptions.merge())
+            val db = Firebase.firestore
+            db.runTransaction { transaction ->
+                val userDocRef = db.collection(USERS_COLLECTION).document(uid)
+                val userNotificationsDocRef = db.collection(NOTIFICATIONS_COLLECTION).document(uid)
+
+                transaction.update(userDocRef, mapOf(ACTIVE_NOTIFICATIONS_FIELD to 0))
+
+                transaction.apply {
+                    if (it.isEmpty()) {
+                        delete(userNotificationsDocRef)
+                    } else {
+                        set(
+                            userNotificationsDocRef,
+                            mapOf(NOTIFICATION_FIELD to remainingNotifications),
+                            SetOptions.merge()
+                        )
+                    }
                 }
             }
         }
