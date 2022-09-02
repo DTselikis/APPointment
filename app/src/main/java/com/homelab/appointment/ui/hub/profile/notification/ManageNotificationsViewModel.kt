408ca7cb2da40d57d24444b9075aeafe16de515b
Dimitris Tselikis <dimitriostselikis@gmail.com>
2022-09-02 12:34:10 +0300

    Find witch notifications are older than 1 day

--- app/src/main/java/com/homelab/appointment/ui/hub/profile/notification/ManageNotificationsViewModel.kt
+++ app/src/main/java/com/homelab/appointment/ui/hub/profile/notification/ManageNotificationsViewModel.kt
@@ -4,18 +4,24 @@ import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.google.firebase.Timestamp
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.firestore.ktx.toObject
 import com.google.firebase.ktx.Firebase
 import com.homelab.appointment.data.NOTIFICATIONS_COLLECTION
 import com.homelab.appointment.model.Notification
 import com.homelab.appointment.model.helper.NotificationsList
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class ManageNotificationsViewModel : ViewModel() {
     private val _notificationsForDisplay = MutableLiveData<List<Notification>>()
     val notificationsForDisplay: LiveData<List<Notification>> = _notificationsForDisplay
 
     private val notifications = mutableListOf<Notification>()
+
+    private lateinit var notificationsToBeDeleted: List<Notification>
 
     fun fetchNotifications(uid: String) {
         Firebase.firestore.collection(NOTIFICATIONS_COLLECTION).document(uid)
@@ -27,9 +33,22 @@ class ManageNotificationsViewModel : ViewModel() {
                         .sortedByDescending { it.timestamp!!.seconds }
                 )
                 _notificationsForDisplay.value = notifications.toList()
+
+                viewModelScope.launch(Dispatchers.IO) {
+                    notificationsToBeDeleted = notifications.olderThan(1)
+                }
             }
             .addOnFailureListener {
                 Log.i("Notifications", "fetchNotifications: There are no notifications")
             }
+    }
+
+    private fun List<Notification>.olderThan(days: Int): List<Notification> {
+        val today = Timestamp.now().toDate()
+        val (toBeDeleted, remaining) = this.partition {
+            it.timestamp!!.toDate().time - today.time >= days
+        }
+
+        return toBeDeleted
     }
 }
