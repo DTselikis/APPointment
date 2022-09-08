226882c1c8f0067e98dbf27601cb59a95d11f2b7
Dimitris Tselikis <dimitriostselikis@gmail.com>
2022-09-08 14:08:14 +0300

    Added lighter background to current date hours

--- app/src/main/java/com/homelab/appointment/ui/hub/info/BindingAdapters.kt
+++ app/src/main/java/com/homelab/appointment/ui/hub/info/BindingAdapters.kt
@@ -1,14 +1,20 @@
 package com.homelab.appointment.ui.hub.info
 
+import android.os.Build
 import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.content.res.ResourcesCompat
 import androidx.databinding.BindingAdapter
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.card.MaterialCardView
 import com.google.android.material.textview.MaterialTextView
 import com.homelab.appointment.R
+import com.homelab.appointment.data.DayOfWeek
 import com.homelab.appointment.data.daysOfWeek
 import com.homelab.appointment.data.orderToDayOfWeek
 import com.homelab.appointment.model.ContactProviderInfo
 import com.homelab.appointment.model.helper.DayOpeningHours
+import java.time.LocalDate
+import java.util.*
 
 @BindingAdapter("openingHours")
 fun bindOpeningHours(recyclerView: RecyclerView, openingHours: List<DayOpeningHours>?) {
@@ -48,6 +54,29 @@ fun bindHours(materialTextView: MaterialTextView, dayOpeningHours: DayOpeningHou
 }
 
 @BindingAdapter("background")
+fun bindBackground(materialCardView: MaterialCardView, order: Int?) {
+    order?.let {
+        val dayOfWeek = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            LocalDate.now().dayOfWeek.value
+        } else {
+            Calendar.getInstance(Locale.getDefault()).dayOfWeek()
+        }
+
+        if (dayOfWeek == it) {
+            materialCardView.apply {
+                setCardBackgroundColor(
+                    ResourcesCompat.getColor(
+                        resources,
+                        R.color.md_theme_dark_primaryContainer,
+                        context.theme
+                    )
+                )
+            }
+        }
+    }
+}
+
+@BindingAdapter("background")
 fun bindBackground(constraintLayout: ConstraintLayout, contactProviderInfo: ContactProviderInfo?) {
     contactProviderInfo?.let {
         if (it.background == null) {
@@ -56,4 +85,14 @@ fun bindBackground(constraintLayout: ConstraintLayout, contactProviderInfo: Cont
             constraintLayout.background = it.background
         }
     }
+}
+
+private fun Calendar.dayOfWeek(): Int = when (this.get(Calendar.DAY_OF_WEEK)) {
+    Calendar.MONDAY -> DayOfWeek.MONDAY.code
+    Calendar.TUESDAY -> DayOfWeek.TUESDAY.code
+    Calendar.WEDNESDAY -> DayOfWeek.WEDNESDAY.code
+    Calendar.THURSDAY -> DayOfWeek.THURSDAY.code
+    Calendar.FRIDAY -> DayOfWeek.FRIDAY.code
+    Calendar.SATURDAY -> DayOfWeek.SATURDAY.code
+    else -> DayOfWeek.SUNDAY.code
 }
